c7985ff651311ffadc152abf5cc6cdd7af289090
Tom Hill <gituser@thomashillconsulting.com>
2025-06-14 13:07:52 -0700

    fixed missing single quotes and commas

--- testconnect.py
+++ testconnect.py
@@ -4,10 +4,10 @@ from netmiko import ConnectHandler
 from getpass import getpass
 
 net_connection = ConnectHandler(
-    host=cisco3.lasthop.io, 
+    host='cisco3.lasthop.io', 
     username='pyclass', 
     password=getpass(), 
-    device_type='cisco_ios'
+    device_type='cisco_ios',
     session_log='my_session.txt',
 )
 print(net_connection.find_prompt())
