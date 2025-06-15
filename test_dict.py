ac7ce20a377b366fd03e3d13facc78b966024d9f
Tom Hill <gituser@thomashillconsulting.com>
2025-06-15 09:40:51 -0400

    Updated test_dict.py

--- test_dict.py
+++ test_dict.py
@@ -3,13 +3,24 @@
 from netmiko import ConnectHandler
 from getpass import getpass
 
-net_connection = ConnectHandler(
-    host='nxos1.lasthop.io', 
-    username='pyclass', 
-    password=getpass(), 
-    device_type='cisco_nxos',
-    session_log='my_session.txt',
-)
+'''
+cisco_881 = {
+    'device_type': 'cisco_ios',
+    'host':   '10.10.10.10',
+    'username': 'test',
+    'password': 'password',
+    'port' : 8022,          # optional, defaults to 22
+    'secret': 'secret',     # optional, defaults to ''
+}
+'''
+device1 = {
+    'host': 'nxos1.lasthop.io', 
+    'username': 'pyclass', 
+    'password': getpass(), 
+    'device_type': 'cisco_nxos',
+   # 'session_log':'my_session.txt',
+    }
+net_connection = ConnectHandler(**device1)
 print(net_connection.find_prompt())
 
     
