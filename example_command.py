e5ca3f2a00f16148361720dce6f6a73c9a1fa49b
Tom Hill <gituser@thomashillconsulting.com>
2025-06-21 10:05:49 -0400

    example to delete file

---
+++ example_command.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python
+
+from netmiko import ConnectHandler
+from getpass import getpass
+
+'''
+cisco_881 = {
+    'device_type': 'cisco_ios',
+    'host':   '10.10.10.10',
+    'username': 'test',
+    'password': 'password',
+    'port' : 8022,          # optional, defaults to 22
+    'secret': 'secret',     # optional, defaults to ''
+}
+'''
+# build a dictionary to hold the device connection information
+"cisco3 { #(Cisco IOS-XE)
+    hostname" = 'cisco3.lasthop.io',
+    "snmp_port" = 161,
+    "ssh_port" = 22,
+    "username" = 'pyclass',
+    "password" = getpass(),
+}
+command = 'delete flash:/sample.txt'
+    net_connection = ConnectHandler(**cisco3)
+    
+    # print the router prompt back from this device to verify you are connecting to the device properly.
+    print(net_connection.find_prompt())
+    send_command(command, expect_string=r'confirm') 
+    send command('y', expect_string=r'#')
+    
+    
+
+    
