d6e011a06a78c591ef8d94afb8b5d9e6bc25f8d1
Tom Hill <gituser@thomashillconsulting.com>
2025-06-21 10:16:47 -0400

    minor command errors

--- example_command.py
+++ example_command.py
@@ -15,19 +15,20 @@ cisco_881 = {
 '''
 # build a dictionary to hold the device connection information
 cisco3 = {          #(Cisco IOS-XE)
-    "hostname" : 'cisco3.lasthop.io',
-    "snmp_port" : 161,
-    "ssh_port" : 22,
-    "username" : 'pyclass',
-    "password" : getpass(),
+    "host": 'cisco3.lasthop.io',
+    "snmp_port": 161,
+    "ssh_port": 22,
+    "username": 'pyclass',
+    "password": getpass(),
+    "device_type": 'cisco_ios',
 }
 command = 'delete flash:/sample.txt'
     net_connection = ConnectHandler(**cisco3)
     
     # print the router prompt back from this device to verify you are connecting to the device properly.
     print(net_connection.find_prompt())
-    send_command(command, expect_string=r'confirm') 
-    send command('y', expect_string=r'#')
+    net_connection.send_command(command, expect_string=r'confirm') 
+    net_connection.send_command('y', expect_string=r'#')
     
     
 
