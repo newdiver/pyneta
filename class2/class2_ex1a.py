f21e976fc5d9099add51b259faa56049a163f0a4
Tom Hill <gituser@thomashillconsulting.com>
2025-06-22 13:58:24 -0400

    changed to use send_command_timimng

--- class2/class2_ex1a.py
+++ class2/class2_ex1a.py
@@ -26,22 +26,16 @@ cisco4 = {
 net_connect = ConnectHandler(**cisco4)
 print(net_connect.find_prompt())
 try:
-    output = net_connect.send_command('ping', expect_string=r'ip', 
-                                      strip_prompt=False,strip_command=False)
-    output += net_connect.send_command('\r', expect_string=r'address',
-                                      strip_prompt=False,strip_command=False)
-    output += net_connect.send_command('8.8.8.8', expect_string=r'count',
-                                       strip_prompt=False,strip_command=False)
-    output += net_connect.send_command('\r', expect_string=r'size',
-                                       strip_prompt=False,strip_command=False)  
-    output += net_connect.send_command('\r', expect_string=r'seconds',
-                                       strip_prompt=False,strip_command=False)                                      
-    output += net_connect.send_command('\r', expect_string=r'commands',
-                                      strip_prompt=False,strip_command=False)
-    output += net_connect.send_command('\r', expect_string=r'sizes', delay_factor=4,
-                                      strip_prompt=False,strip_command=False)
-   # output += net_connect.send_command('', expect_string=r'ms',
-#                                      strip_prompt=False,strip_command=False)
+    output = net_connect.send_command_timing('Ping', strip_prompt=False,strip_command=False)
+    output += net_connect.send_command_timing("\n", strip_prompt=False, strip_command=False)
+    output += net_connect.send_command_timing("8.8.8.8", strip_prompt=False, strip_command=False
+)
+    output += net_connect.send_command_timing("\n", strip_prompt=False, strip_command=False)
+    output += net_connect.send_command_timing("\n", strip_prompt=False, strip_command=False)
+    output += net_connect.send_command_timing("\n", strip_prompt=False, strip_command=False)
+    output += net_connect.send_command_timing("\n", strip_prompt=False, strip_command=False)
+    output += net_connect.send_command_timing("\n", strip_prompt=False, strip_command=False)
+    net_connect.disconnect()
 
     print(output)
 except Exception as e:
