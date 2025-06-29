18e15318fca2a069f706a3491b5562b7a755d980
Tom Hill <gituser@thomashillconsulting.com>
2025-06-29 15:27:25 -0400

    added code to extract neighbor port from lldp neighbor

--- class2/class2_ex3.py
+++ class2/class2_ex3.py
@@ -6,6 +6,7 @@ and 'show lldp neighbors' against the Cisco4 device with use_textfsm=True.
 
 from netmiko import ConnectHandler
 from getpass import getpass
+from pprint import pprint
 
 cisco4 = {
     "device_type": "cisco_ios",
@@ -19,8 +20,11 @@ print(net_connect.find_prompt())
 try:
     output = net_connect.send_command('show version',use_textfsm=True)
     output += net_connect.send_command('show lldp neighbors',use_textfsm=True)
+    neighbor_port = output[1]['neighbor_interface']
 
-    print(output)
+    pprint(output)
+    print(f"The neighbor switchport is {neighbor_port}")
+    
 except Exception as e:
     print(f"An error occurred: {e}")
 
