93a3e4f50ef5e8ed4749d23214ac178a5321609b
Tom Hill <gituser@thomashillconsulting.com>
2025-07-04 09:47:43 -0400

    fixed error

--- class2/class2_ex5.py
+++ class2/class2_ex5.py
@@ -9,7 +9,6 @@ VLANs including VLAN names (just pick 5 VLAN numbers between 100 - 999).
 Use Netmiko's send_config_from_file() method to accomplish this. 
 Also use Netmiko's save_config() method to save the changes to the startup-config.
 '''
-nxos1 = {
 nxos1 = { 
     "host" : 'nxos1.lasthop.io',
     "ssh_port" : 22,
